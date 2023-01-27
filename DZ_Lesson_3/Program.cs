2cc494adca1e3136d4ef71015fe6297f8c2695e0
DrinTin <dmitrievkit@gmail.com>
2023-01-27 15:11:26 +0300

    Add Lesson_4 lesson_6 and DZ_Lesson_3

---
+++ DZ_Lesson_3/Program.cs
@@ -0,0 +1,67 @@
+﻿//Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+//Console.WriteLine("Введите пятизначное число");
+//int a = Convert.ToInt32(Console.ReadLine());
+//while (a < 10000 && a > 99999)
+//{
+//    Console.WriteLine("Введите пятизначное число");
+//    a = Convert.ToInt32(Console.ReadLine());
+//}
+//string s = a.ToString();
+//char[] ar = s.ToCharArray();
+//Array.Reverse(ar);
+//s = new string(ar);
+//int b = Convert.ToInt32(s);
+// if (a == b){
+//    Console.WriteLine($"{a} является полиндромом");
+// }
+//else {
+//    Console.WriteLine($"{a} не является полиндромом");
+//}
+
+//Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+//A (3,6,8); B (2,1,-7), -> 15.84
+
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+//Console.WriteLine("Введите координаты точки A");
+//Console.WriteLine("по X");
+//int x = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("по Y");
+//int y = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("по Z");
+//int z = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите координаты точки B");
+//Console.WriteLine("по X");
+//int x1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("по Y");
+//int y1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("по Z");
+//int z1 = Convert.ToInt32(Console.ReadLine());
+
+//double AB = Math.Sqrt(Math.Pow(x - x1, 2) + Math.Pow(y - y1, 2) + Math.Pow(z - z1, 2));
+
+//Console.WriteLine($"Расстояние между точками = {Math.Round(AB,2)}");
+
+//Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 0){
+    for (int n1 = 0; n1 <= n; n1++)
+    {
+        double n2 = Math.Pow(n1, 2);
+        Console.WriteLine($"Квадрат {n1} = {n2}");
+    }
+}
+else{
+    for (int n1 = 0; n1 >= n; n1--)
+    {
+        double n2 = Math.Pow(n1, 2);
+        Console.WriteLine($"Квадрат {n1} = {n2}");
+    } 
+}
---
+++ Lesson_6/Program.cs
@@ -0,0 +1,230 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+
+
+// void PrintArray(int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             matr[i,j] = new Random().Next(-10,10);
+//             Console.Write($"{matr[i, j]} ");
+//         }
+//     Console.WriteLine();
+//     }
+// }
+// int[,] matrix = new int[3, 4];
+// Console.WriteLine();
+// PrintArray(matrix);
+// Console.WriteLine();
+
+// int[,] GetRandomMatrix(int rows, int columns, int leftRange, int rightRange)
+// {
+//     int[,] matrix = new int[rows, columns];
+
+//     for(int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = new Random().Next(leftRange, rightRange + 1);
+//         }
+//     }
+
+//     return matrix;
+// }
+
+// void PrintMatrix(int[,] matrix)
+// {
+//     for(int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write(matrix[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// const int ROWS = 3;
+// const int COLUMNS = 4;
+// const int LEFT_RANGE = 0;
+// const int RIGHT_RANGE = 10;
+
+// int[,] array = GetRandomMatrix(ROWS, COLUMNS, LEFT_RANGE, RIGHT_RANGE);
+// PrintMatrix(array);
+
+// Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+
+// void PrintMatrix(int[,] matrix)
+// {
+//     for(int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write(matrix[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int[,] GetRandomMatrix(int rows, int columns)
+// {
+//     int[,] matrix = new int[rows, columns];
+
+//     for(int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = i + j;
+//         }
+//     }
+
+//     return matrix;
+// }
+
+// const int ROWS = 3;
+// const int COLUMNS = 4;
+
+// int[,] array = GetRandomMatrix(ROWS, COLUMNS);
+// PrintMatrix(array);
+
+// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+// ​ (в примере подсчет индексов начинается с 1 , как в математике)
+// Например, изначально массив
+// выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// int[,] GetRandomMatrix(int rows, int columns, int leftRange, int rightRange)
+// {
+//     int[,] matrix = new int[rows, columns];
+
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = new Random().Next(leftRange, rightRange + 1);
+//         }
+//     }
+
+//     return matrix;
+// }
+
+// void GetSquareElem(int[,] matrix)
+// {
+//     for (int i = 1; i < matrix.GetLength(0); i += 2)
+//     {
+//         for (int j = 1; j < matrix.GetLength(1); j += 2)
+//         {
+//             matrix[i, j] *= matrix[i, j];
+//         }
+//     }
+// }
+
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write(matrix[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// // void PrintMatrix2(int[,] matrix)
+// // {
+// //     for (int i = 0; i < matrix.GetLength(0); i++)
+// //     {
+// //         for (int j = 0; j < matrix.GetLength(1); j++)
+// //         {
+// //             if (i % 2 != 0 && j % 2 != 0) matrix[i, j] = matrix[i, j] * matrix[i, j];
+// //             Console.Write(matrix[i, j] + " ");
+// //         }
+// //         Console.WriteLine();
+
+// const int ROWS = 5;
+// const int COLUMNS = 7;
+// const int LEFT_RANGE = 0;
+// const int RIGHT_RANGE = 10;
+
+// int[,] resultMatrix = GetRandomMatrix(ROWS, COLUMNS, LEFT_RANGE, RIGHT_RANGE);
+
+// Console.WriteLine();
+// PrintMatrix(resultMatrix);
+// Console.WriteLine();
+// GetSquareElem(resultMatrix);
+// // PrintMatrix2(array);
+// PrintMatrix(resultMatrix);
+// Console.WriteLine();
+
+
+// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+// ​
+// Доп. условие для 51 задачи: сделать суммирование в один цикл. Матрица может быть прямоугольной
+
+
+int[,] GetRandomMatrix(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] matrix = new int[rows, columns];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return matrix;
+}
+
+int GetSumMainDiagonal(int[,] matrix)
+{
+    int result = 0;
+
+    for (int i = 0; i < matrix.GetLength(0) && i < matrix.GetLength(1); i++)
+    {        
+        result += matrix[i, i];        
+    }
+
+    return result;   
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+const int ROWS = 4;
+const int COLUMNS = 7;
+const int LEFT_RANGE = 0;
+const int RIGHT_RANGE = 10;
+
+int[,] resultMatrix = GetRandomMatrix(ROWS, COLUMNS, LEFT_RANGE, RIGHT_RANGE);
+Console.WriteLine();
+PrintMatrix(resultMatrix);
+int sum = GetSumMainDiagonal(resultMatrix);
+Console.WriteLine();
+Console.WriteLine(sum);
+Console.WriteLine();
